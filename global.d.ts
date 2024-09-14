17c4183d39b47a8a9528752c4832ee911cc333f3
mck-s <mck-s@outlook.jp>
2024-09-14 16:43:00 +0900

    forgot to commit no yatsu

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        API_URL: string;
+        API_KEY: string;
+    }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import dotenv from 'dotenv'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App'
 import theme from './theme'
 import './index.css'
+
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
